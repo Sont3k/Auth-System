de54a2936440ace020f17f4a61842a14117c8dd3
Kyrylo Vovkotrub <markus.residence@gmail.com>
2023-04-28 20:44:29 +0300

    feat: install auth package

--- Assets/_App/Scripts/AuthSystem.cs
+++ Assets/_App/Scripts/AuthSystem.cs
@@ -1,9 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _App.Scripts
 {
     public class AuthSystem : MonoBehaviour
     {
+        [SerializeField] private Button _authButton;
+
+        private void OnEnable()
+        {
+            _authButton.onClick.AddListener(HandleAuthButton);
+        }
+
+        private void OnDisable()
+        {
+            _authButton.onClick.RemoveAllListeners();
+        }
         
+        private void HandleAuthButton()
+        {
+            
+        }
     }
 }
